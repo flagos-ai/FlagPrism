8ae523c8e50fee985ac836c0734b014b66a9a53f
ChijinZ <tlock.chijin@gmail.com>
2026-08-17 18:56:53 +0800

    [Profiler] Preserve generated include dependency order

--- Profiler/Dialect/include/Dialect/Proton/IR/Dialect.h
+++ Profiler/Dialect/include/Dialect/Proton/IR/Dialect.h
@@ -1,12 +1,13 @@
 #ifndef DIALECT_PROTON_IR_DIALECT_H_
 #define DIALECT_PROTON_IR_DIALECT_H_
 
-#include "Profiler/Dialect/include/Dialect/Proton/IR/Dialect.h.inc"
-#include "Profiler/Dialect/include/Dialect/Proton/IR/OpsEnums.h.inc"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/PatternMatch.h"
+
+#include "Profiler/Dialect/include/Dialect/Proton/IR/Dialect.h.inc"
+#include "Profiler/Dialect/include/Dialect/Proton/IR/OpsEnums.h.inc"
 
 #define GET_OP_CLASSES
 #include "Profiler/Dialect/include/Dialect/Proton/IR/Ops.h.inc"
